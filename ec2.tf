06e4b74b73c596b45ed6c14f067f3fd3546b2249
vammy55 <vamsee54@gmail.com>
2020-06-10 22:23:33 +0530

    four

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-
+   region = "us-east-1"
 }
 
 resource "aws_instance" "ec2-server" {
@@ -12,24 +12,9 @@ resource "aws_instance" "ec2-server" {
     private_key = file("./terraform-key.pem")
     host     = self.public_ip
   }
-  provisioner "remote-exec" {
-    inline = [
-      "sudo pip install ansible",
-      "sudo yum install java-1.8.0 -y",
-      "sudo yum remove java-1.7.0-openjdk -y",
-      "sudo yum install wget unzip git -y",
-      "sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo",
-      "sudo rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key",
-      "sudo yum install jenkins -y",
-      "sudo service jenkins start",
-      "sudo chkconfig --add jenkins",
-      "sudo wget https://releases.hashicorp.com/terraform/0.12.21/terraform_0.12.21_linux_amd64.zip",
-      "sudo unzip /home/ec2-user/terraform_0.12.21_linux_amd64.zip",
-      "sudo mv /home/ec2-user/terraform /usr/bin"
-    ]
-  }
+  
   tags = {
-    Name = "Jenkins-Server-terraform1"
+    Name = "Jenkins-Server-terraform"
   }       
   }
 
